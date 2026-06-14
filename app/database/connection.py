1886caf9625d60a8a6dfabc25c6db522f6b9a5da
Arseniy <arseniy.burtsev@gmail.com>
2026-06-14 20:40:02 +0300

    people repo

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,11 +1,20 @@
 import sqlite3
+from collections.abc import Iterator
+from contextlib import contextmanager
 from pathlib import Path
 
 DB_PATH = Path(__file__).resolve().parents[2] / "data" / "birthdays.db"
 
-def get_connection():
-    connection =  sqlite3.connect(DB_PATH)
-    connection.row_factory = sqlite3.Row
-    connection.execute("PRAGMA foreign_keys = ON")
 
-    return connection
+@contextmanager
+def get_connection() -> Iterator[sqlite3.Connection]:
+    connection = sqlite3.connect(DB_PATH)
+
+    try:
+        connection.row_factory = sqlite3.Row
+        connection.execute("PRAGMA foreign_keys = ON")
+
+        with connection:
+            yield connection
+    finally:
+        connection.close()
---
+++ app/repositories/people_repository.py
@@ -0,0 +1,39 @@
+from app.database.connection import get_connection
+
+
+def add_person(
+        user_id: int,
+        full_name: str,
+        day: int,
+        month: int,
+        year: int | None,
+        notes: str | None
+) -> int:
+    with get_connection() as conn:
+        cursor = conn.execute("INSERT INTO people ("
+                     "user_id,"
+                     "full_name,"
+                     "birth_day,"
+                     "birth_month,"
+                     "birth_year,"
+                     "notes"
+                     ")"
+                     "VALUES (?, ?, ?, ?, ?, ?)",
+                     (user_id, full_name, day, month, year, notes))
+        return cursor.lastrowid
+
+def get_people(user_id: int):
+    with get_connection() as conn:
+        return conn.execute("SELECT * FROM people "
+                     "WHERE user_id = ?", (user_id,)).fetchall()
+
+def get_person(user_id: int, person_id: int):
+    with get_connection() as conn:
+        return conn.execute("SELECT * FROM people "
+                              "WHERE id = ? AND user_id = ?", (person_id, user_id)).fetchone()
+
+def delete_person(user_id: int, person_id: int) -> bool:
+    with get_connection() as conn:
+        cursor = conn.execute("DELETE FROM people "
+                     "WHERE id = ? AND user_id = ?", (person_id, user_id))
+        return cursor.rowcount > 0
