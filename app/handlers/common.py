d5c37464870831fe9ebdb49a654c31193b8df742
Arseniy <arseniy.burtsev@gmail.com>
2026-06-14 19:59:34 +0300

    registration

--- app/handlers/common.py
+++ app/handlers/common.py
@@ -1,6 +1,10 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
+from app.repositories.user_repository import create_or_update_user
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    telegram_user, chat = update.effective_user, update.effective_chat
+    create_or_update_user(telegram_user.id, chat.id, telegram_user.username)
     await update.message.reply_text("Вы зарегистрированы!")
--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,6 +1,18 @@
+from app.database.connection import get_connection
+
 def create_or_update_user(
         telegram_user_id: int,
         notification_chat_id: int,
         username: str | None
 ):
-    pass
+    with get_connection() as conn:
+        conn.execute("INSERT INTO users ("
+                     "telegram_user_id,"
+                     "notification_chat_id,"
+                     "username"
+                     ")"
+                     "VALUES (?, ?, ?)"
+                     "ON CONFLICT(telegram_user_id) DO UPDATE SET"
+                     "  notification_chat_id = excluded.notification_chat_id,"
+                     "  username = excluded.username",
+                     (telegram_user_id, notification_chat_id, username))
